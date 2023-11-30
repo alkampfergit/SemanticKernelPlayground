67bef2166fb0191819e505cad7ff6678d687be4f
gian maria ricci <ricci.gianmaria@gmail.com>
2023-11-30 20:38:45 +0100

    More on SK

--- 03_functions/helpers/LoggingHelper.py
+++ 03_functions/helpers/LoggingHelper.py
@@ -2,14 +2,14 @@ import logging
 from logging import Logger
 
 class LoggingHelper:
-    def get_logger(self, name: str) -> Logger:
+    def get_logger(self, name: str, level:int) -> Logger:
         """
         Returns a logger object with a console handler
         """
         logger = logging.getLogger(name)
-        logger.setLevel(logging.DEBUG)
+        logger.setLevel(level)
         console_handler = logging.StreamHandler()
-        console_handler.setLevel(logging.DEBUG)
+        console_handler.setLevel(level)
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         console_handler.setFormatter(formatter)
         logger.addHandler(console_handler)
--- 03_functions/plugins/JarvisPlugin/Tasks.py
+++ 03_functions/plugins/JarvisPlugin/Tasks.py
@@ -26,3 +26,60 @@ class Tasks:
 
         # Api will return the new version of the task, so we can update the context
         return "7"
+
+    @sk_function(
+        description="Search task with full text string",
+        name="SearchTask"
+    )
+    @sk_function_context_parameter(
+        name="search_string",
+        description="The string to search"
+    )
+    def search_task(self, search_string: str) -> str:
+        print(f"Searching for task: {search_string}")
+        # Here you should call the API to search for a task
+        # api will return a list of tasks
+        tasks = "Task_3, Task_4, Task_6"
+        return tasks
+    
+    @sk_function(
+        description="Load task detail in json format",
+        name="LoadTask"
+    )
+    @sk_function_context_parameter(
+        name="task_id",
+        description="The id of the task to load"
+    )  
+    def load_task(self, task_id: str) -> str:
+        """
+        Load a task from the API and return the task content in json format
+        """
+        print(f"Loading task {task_id}")
+        # Here you should call the API to load a task
+        # api will return the task content
+        tasks = {
+            "Task_3": {
+                "id": "Task_3",
+                "title": "Hey I'm task 3",
+                "description": "This is the description of task 3",
+                "version": 5,
+                "due_date": "2021-10-10"
+            },
+            "Task_4": {
+                "id": "Task_4",
+                "title": "I'm beautiful task 4",
+                "description": "This is the description of task 4",
+                "version": 5,
+                "due_date": "2022-10-10"
+            },
+            "Task_6": {
+                "id": "Task_6",
+                "title": "I'm the six",
+                "description": "This is the description of task 6",
+                "version": 5,
+                "due_date": "2023-10-10"
+            }
+        }
+        task = tasks.get(task_id)
+        # return task in json format
+        return str(task)
